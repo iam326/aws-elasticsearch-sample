aeb1339c791d1237a6d11d7f5b61bba06e192663
t.takahashi <takahashi.developer@gmail.com>
2020-02-04 01:07:23 +0900

    improve deploy process

--- lambda/deploy.sh
+++ lambda/deploy.sh
@@ -5,21 +5,26 @@ set -euo pipefail
 readonly PROJECT_NAME="aws-elasticsearch-sample"
 readonly STACK_NAME="${PROJECT_NAME}-lambda"
 readonly TEMPLATE_FILE="$(pwd)/template.yaml"
-readonly BUCKET_NAME="s3://iam326.${PROJECT_NAME}"
+readonly BUCKET_NAME="iam326.${PROJECT_NAME}"
 
-# aws s3 mb ${BUCKET_NAME}
 cd ./src
-npx tsc ./index.ts
-zip -j ./src.zip ./*
-aws s3 cp ./src.zip "${BUCKET_NAME}/src.zip"
-rm ./src.zip
-cd ..
+npx tsc
+cp ./package.json ./dist
+cp ./yarn.lock ./dist
+yarn --cwd ./dist install --production
 
 aws cloudformation validate-template \
   --template-body "file://${TEMPLATE_FILE}"
 
+aws cloudformation package \
+    --template-file ${TEMPLATE_FILE} \
+    --s3-bucket ${BUCKET_NAME} \
+    --output-template-file packaged-template.yml
+
 aws cloudformation deploy \
   --stack-name ${STACK_NAME} \
-  --template-file ${TEMPLATE_FILE} \
+  --template-file packaged-template.yml \
   --capabilities CAPABILITY_NAMED_IAM \
   --tags date="$(date '+%Y%m%d%H%M%S')"
+
+rm packaged-template.yml
