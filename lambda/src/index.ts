a8bb8100e2f12c14788631d73b3e5b14f24456e6
t.takahashi <takahashi.developer@gmail.com>
2020-02-05 00:17:39 +0900

    connection trial code (failed)

--- lambda/src/index.ts
+++ lambda/src/index.ts
@@ -1,6 +1,5 @@
 'use strict';
 
-/*
 const AWS = require('aws-sdk');
 AWS.config.update({region: 'ap-northeast-1'});
 
@@ -32,14 +31,25 @@ function connectElasticsearch(host: string) {
     connectionClass: require('http-aws-es')
   });
 }
-*/
 
 exports.handler = async (event: any, context: any, callback: Function) => {
   const stackName = 'aws-elasticsearch-sample-stack';
   const outputKeyName = 'ElasticSearchDomainEndpoint';
-  //const endpoint = await getESDomainEndpoint(stackName, outputKeyName);
-  //const es = connectElasticsearch(endpoint);
-  //console.log(es);
+  const endpoint = await getESDomainEndpoint(stackName, outputKeyName);
+  const es = connectElasticsearch(endpoint);
+
+  try {
+    es.index({
+      index: "_sample",
+      body: {
+        title: 'foo',
+        description: 'bar'
+      }
+    });
+    console.log('success!');
+  } catch (e) {
+    console.log(e);
+  }
 
   return callback(null, 'Success!');
 }
