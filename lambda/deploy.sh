ff0af3cf6208f54a35618435fc79d1e9394edda6
t.takahashi <takahashi.developer@gmail.com>
2020-02-06 23:47:58 +0900

    add lambda environment

--- lambda/deploy.sh
+++ lambda/deploy.sh
@@ -7,6 +7,12 @@ readonly STACK_NAME="${PROJECT_NAME}-lambda"
 readonly TEMPLATE_FILE="$(pwd)/template.yaml"
 readonly BUCKET_NAME="iam326.${PROJECT_NAME}"
 
+ES_ENDPOINT=$(aws cloudformation describe-stacks \
+  --stack-name "${PROJECT_NAME}-es" \
+  --query 'Stacks[].Outputs[?OutputKey==`ElasticSearchDomainEndpoint`].OutputValue' \
+  --output text
+)
+
 cd ./src
 npx tsc
 
@@ -15,6 +21,7 @@ cp ./package.json ./yarn.lock ./nodejs
 yarn --cwd ./nodejs install --production --force
 zip -r layer.zip ./nodejs
 aws s3 cp ./layer.zip "s3://${BUCKET_NAME}"
+rm layer.zip
 
 aws cloudformation validate-template \
   --template-body "file://${TEMPLATE_FILE}"
@@ -29,6 +36,7 @@ aws cloudformation deploy \
   --template-file packaged-template.yml \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides \
-    BucketName=${BUCKET_NAME}
+    BucketName=${BUCKET_NAME} \
+    ElasticsearchEndpoint="https://"${ES_ENDPOINT}
 
 rm packaged-template.yml
