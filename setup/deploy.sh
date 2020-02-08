85c8e09e7a91d1accbc8a6a9317f718923738c50
t.takahashi <takahashi.developer@gmail.com>
2020-02-08 23:59:07 +0900

    add setup template

---
+++ setup/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -euo pipefail
+
+readonly PROJECT_NAME="aws-elasticsearch-sample"
+readonly STACK_NAME="${PROJECT_NAME}-setup"
+readonly TEMPLATE_FILE="$(pwd)/template.yaml"
+readonly BUCKET_NAME="iam326.${PROJECT_NAME}"
+
+aws cloudformation validate-template \
+  --template-body "file://${TEMPLATE_FILE}"
+
+aws cloudformation deploy \
+  --stack-name ${STACK_NAME} \
+  --template-file ${TEMPLATE_FILE} \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides \
+    BucketName=${BUCKET_NAME}
