b66356d6ae7af4b8e492abc0481637018cca56a3
t.takahashi <takahashi.developer@gmail.com>
2020-02-11 21:55:33 +0900

    add error message

--- lambda/src/utils.ts
+++ lambda/src/utils.ts
@@ -9,13 +9,13 @@ AWS.config.update({region: 'ap-northeast-1'});
 export function getDynamoDocClient() {
   const env_name = process.env.ENV_NAME;
   if (!env_name) {
-    throw 'error';
+    throw 'ENV_NAME is not defined.';
   }
   const params: any = { region: 'ap-northeast-1' };
   if (env_name === 'local') {
     const endpoint = process.env.DB_ENDPOINT;
     if (!endpoint) {
-      throw 'error';
+      throw 'DB_ENDPOINT is not defined.';
     }
     params.endpoint = endpoint;
   }
@@ -26,7 +26,7 @@ export function connectElasticsearch() {
   const env_name = process.env.ENV_NAME;
   const endpoint = process.env.ES_ENDPOINT;
   if (!env_name || !endpoint) {
-    throw 'error';
+    throw 'ENV_NAME or ES_ENDPOINT is not defined.';
   }
 
   let params: any = { node: endpoint };
